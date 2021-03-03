ded1a003bd4e4aa75ae71b5776c085a4efea9063
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-03 17:51:17 -0500

    Finished lab.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
+import React, {Component} from 'react'
 // Code EyesOnMe Component Here
+
+export default class EyesOnMe extends Component{
+
+  logFocus = () => {
+    console.log('Good!')
+  }
+
+  logBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  render(){
+    return (
+      <button onFocus={this.logFocus} onBlur={this.logBlur} >Button</button>
+    )
+  }
+}
